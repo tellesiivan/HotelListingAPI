6ff5225351af2610b7ade61d52d96a786336e931
ivan telles <tellezivan7@gmail.com>
2023-11-07 18:55:46 -0800

    feat: setup serilog

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS support(has to be above .Build())
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policyBuilder => 
+        policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+});
+
+// serilog 
+// context --> Instance of the builder
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.WriteTo.Console()
+    .ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
@@ -16,9 +30,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// serilog
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
